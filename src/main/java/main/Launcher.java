c864a70aa1f74ec95c675bf434b8f6af2a5b9c38
Francesco Magno <francesco@magno.cc>
2020-11-19 00:36:00 +0100

    better help formatting

--- src/main/java/main/Launcher.java
+++ src/main/java/main/Launcher.java
@@ -7,6 +7,8 @@ public class Launcher {
     public static void main(String[] args) {
         CommandLine cl = new CommandLine(new JSqlBenchCommand());
         cl.setCaseInsensitiveEnumValuesAllowed(true);
+        cl.setUsageHelpWidth(150);
+        cl.setUsageHelpLongOptionsMaxWidth(50);
         int rc = cl.execute(args);
         System.exit(rc);
     }
